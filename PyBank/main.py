dd0d2f7405ef02fe5bc306af1bc6b53f638044d5
Timothy Kelley <thepianist86@gmail.com>
2021-07-11 22:51:57 -0500

    added greatest increase and greatest decrease

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,26 +1,34 @@
 # import budget data
 import os
 import csv
+from operator import itemgetter
 
 
 fpath = os.path.join("Resources", "budget_data.csv")
 
-net_tot_amt = 0
+bd_rows = {}
 months = 0
+net_tot_amt = 0
 change = 0
+change_list = {}
 change_tot = 0
 aver_change = 0
+greatest_inc = {}
+greatest_dec = {}
 
-
-# The total number of months included in the dataset
 with open(fpath) as bd_csv:
     bd_reader = csv.reader(bd_csv, delimiter=",")
-    months = len(list(bd_reader)) - 1
+    for r in bd_reader:
+        bd_rows[r[0]] = r[1]
+
+# The total number of months included in the dataset
+    months = len(list(bd_rows)) - 1
     print (f"{months}")
 
 # The net total amount of "Profit/Losses" over the entire period
     bd_csv.seek(0)
     next(bd_reader)
+
     net_tot_amt = sum(int(r[1]) for r in bd_reader)
     print(net_tot_amt)
 
@@ -34,15 +42,21 @@ with open(fpath) as bd_csv:
         else:         
             second = int(r[1])
             change = second - first
+            change_list[count] = change
             change_tot += change
             first = second
         count += 1
-    aver_change = change_tot/months
+    aver_change = round(change_tot/(months-1), 2)
     print(aver_change)
 
-
-
-
 # The greatest increase in profits (date and amount) over the entire period
+    amount = max(change_list.items(), key=itemgetter(1))
+    date = list(bd_rows)
+    greatest_inc[date[amount[0]+1]] = amount[1]
+    print(greatest_inc)
 
 # The greatest decrease in profits (date and amount) over the entire period
+    amount = min(change_list.items(), key=itemgetter(1))
+    date = list(bd_rows)
+    greatest_dec[date[amount[0]+1]] = amount[1]
+    print(greatest_dec)
